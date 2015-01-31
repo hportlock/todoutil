1e96cde503e8baaec946888c9dc904273e0e827e
Rome Portlock <hportlock@gmail.com>
2015-01-30 22:11:38 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Todoutil
+
+Todoutil is a python program that uploads todo.txt formatted files into
+todoist.
+
+## Installation
+
+Clone this git repository and then install the requirements with pip:
+```bash
+git clone https://github.com/hportlock/todoutil.git
+pip install -r requirements.txt
+```
+
+## Usage
+
+Run the script, it will ask for the path to your todo.txt file and your API
+token from todoist. Your API token can be found by logging into the todoist
+web site and looking under the account tab of settings.
+
+```bash
+./txttodoist.py
+```
+
+## Questions and Suggestions
+
+If you have a problem or an suggestion open a github issue to discuss it. 
---
+++ test_todoutil.py
@@ -0,0 +1,77 @@
+#! /usr/bin/env python
+
+import unittest
+
+from todoutil import parse_task
+from todoutil import Task
+
+class TestParseTodo(unittest.TestCase):
+
+  def test_parse_priority(self):
+    task = parse_task('(A) Call Mom')
+    self.assertEqual(task.priority, 'A')
+    self.assertEqual(task.content, 'Call Mom')
+
+  def test_parse_no_priority(self):
+    task = parse_task('Really gotta call Mom (A) @phone @someday')
+    self.assertIsNone(task.priority)
+    task = parse_task('(b) Get back to the boss')
+    self.assertIsNone(task.priority)
+    task = parse_task('(B)->Submit TPS report')
+    self.assertIsNone(task.priority)
+
+  def test_parse_creation_date(self):
+    task = parse_task('2011-03-02 Document +TodoTxt task format')
+    self.assertEqual(task.creation_date, '2011-03-02')
+
+  def test_parse_creation_date_with_pri(self):
+    task = parse_task('(A) 2011-03-02 Call Mom')
+    self.assertEqual(task.creation_date, '2011-03-02')
+
+  def test_parse_no_creation_date(self):
+    task = parse_task('(A) Call Mom 2011-03-02')
+    self.assertIsNone(task.creation_date)
+
+  def test_parse_project(self):
+    line = '(A) Call Mom +Family +PeaceLoveAndHappiness @iphone @phone'
+    task = parse_task(line)
+    self.assertListEqual(
+        sorted(task.projects),
+        sorted(['Family', 'PeaceLoveAndHappiness']))
+
+  def test_parse_no_project(self):
+    task = parse_task('Learn how to add 2+2')
+    self.assertEqual(task.projects, [])
+
+  def test_parse_context(self):
+    line = '(A) Call Mom +Family +PeaceLoveAndHappiness @iphone @phone'
+    task = parse_task(line)
+    self.assertListEqual(
+        sorted(task.contexts),
+        sorted(['iphone', 'phone']))
+
+  def test_parse_no_context(self):
+    task = parse_task('Email SoAndSo at soandso@example.com')
+    self.assertEqual(task.contexts, [])
+
+  def test_parse_complete(self):
+    task = parse_task('x 2011-03-03 Call Mom')
+    self.assertEqual(task.completion_date, '2011-03-03')
+
+  def test_parse_not_complete(self):
+    task = parse_task('xylophone lesson')
+    self.assertIsNone(task.completion_date)
+
+    task = parse_task('X 2012-01-01 Make resolutions')
+    self.assertIsNone(task.completion_date)
+
+    task = parse_task('(A) x Find ticket prices')
+    self.assertIsNone(task.completion_date)
+
+  def test_parse_content(self):
+    line = '(A) Call Mom +Family +PeaceLoveAndHappiness @iphone @phone'
+    task = parse_task(line)
+    self.assertEqual(task.content, 'Call Mom')
+
+if __name__ == "__main__":
+  unittest.main()
